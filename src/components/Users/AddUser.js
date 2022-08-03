e775bb9cd3d6911b3df53b31e5d77139c9a95b35
Ilie1985 <ilie.marian32@yahoo.com>
2022-08-03 14:31:20 +0100

    ErrorModal component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 import { useState } from "react";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
@@ -34,30 +35,38 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className={styles.input}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          onChange={userNameChangeHandler}
-          value={enteredUserName}
-        />
+    <div>
+      <ErrorModal
+        title="An Error Ocured!"
+        message="Something Went 
+    Wrong!"
+      />
 
-        <label htmlFor="age">Age</label>
-        <input
-          type="number"
-          id="age"
-          min="1"
-          max="100"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button className={styles.button} type="submit">
-          Add User
-        </Button>
-      </form>
-    </Card>
+      <Card className={styles.input}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={userNameChangeHandler}
+            value={enteredUserName}
+          />
+
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            min="1"
+            max="100"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button className={styles.button} type="submit">
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
